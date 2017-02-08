0e56efcf5879e7d013cb9c8cd1328ea81991e057
Fernando Rocha <fernandorsl@gmail.com>
2017-02-08 17:13:56 -0600

    including normale and magnitude functions

--- vector.py
+++ vector.py
@@ -31,3 +31,30 @@ class Vector(object):
     def times_scalar(self, c):
         new_coordinates = [c*x for x in self.coordinates]
         return Vector(new_coordinates)
+
+    def magnitude(self):
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return math.sqrt(sum(coordinates_squared))
+
+    def normalize(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
+
+
+import math
+
+v = Vector([-0.221, 7.437])
+print "1 - %s" %v.magnitude()
+
+v1 = Vector([8.813, -1.331, -6.247])
+print "2 - %s" % v1.magnitude()
+
+v2 = Vector([5.581, -2.136])
+print "3 - %s" % v2.normalize()
+
+v3 = Vector([1.996, 3.108, -4.554])
+print "4 - %s" % v3.normalize()
