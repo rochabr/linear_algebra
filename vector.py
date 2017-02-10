2ff49538ba06d279b6d54f6c54284a5699ccce4f
Fernando Rocha <fernandorsl@gmail.com>
2017-02-10 16:56:18 -0600

    Including dot product and angle_with

--- vector.py
+++ vector.py
@@ -1,9 +1,17 @@
+import math
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
 class Vector(object):
+    
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -39,14 +47,33 @@ class Vector(object):
     def normalize(self):
         try:
             magnitude = self.magnitude()
-            return self.times_scalar(1./magnitude)
+            return self.times_scalar(Decimal('1.0')/magnitude)
         except ZeroDivisionError:
             raise Exception('Cannot normalize the zero vector')
 
+    def dot_product(self, v):
+        return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalize()
+            u2 = v.normalize()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if (in_degrees):
+                degrees_per_radian = 180. / pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot comoute an angle with the zero vector')
+            else:
+                raise e
 
 
-import math
-
+'''
 v = Vector([-0.221, 7.437])
 print "1 - %s" %v.magnitude()
 
@@ -58,3 +85,13 @@ print "3 - %s" % v2.normalize()
 
 v3 = Vector([1.996, 3.108, -4.554])
 print "4 - %s" % v3.normalize()
+'''
+v = Vector([7.887, 4.138])
+print "1 - %s" % v.dot_product(Vector([-8.802, 6.776]))
+
+v2 = Vector([-5.955, -4.904, -1.874])
+print "2 - %s" % v2.dot_product(Vector([-4.496, -8.755, 7.103]))
+
+v3 = Vector([3.183, -7.627])
+print "3 - %s" % v3.angle_with(Vector([-2.668, 5.319]))
+
